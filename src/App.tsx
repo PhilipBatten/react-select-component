74b82837ab0c13f2fd8ad2187f14f16336e2b61c
Philip Batten <philip.batten@efficioconsulting.com>
2023-04-20 16:49:18 +0100

    empty by default

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ const options = [
   { label: 'First', value: 1},
   { label: 'Second', value: 2},
   { label: 'Third', value: 3},
-  { label: 'Fourth', value: 4}
+  { label: 'Fourth', value: 4},
 ]
 
 function App() {
-  const [value1, setValue1] = useState<SelectOption[]>([options[0]])
+  const [value1, setValue1] = useState<SelectOption[]>([])
   const [value2, setValue2] = useState<SelectOption | undefined>(options[0])
   return  <>
     <Select multiple options={options} value={value1} onChange={o => setValue1(o)}/>
