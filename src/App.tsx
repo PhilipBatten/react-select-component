084aa65ccf0b2feb67c1e561fdaad9e1badd52b9
Philip Batten <philip.batten@efficioconsulting.com>
2023-04-20 13:57:10 +0100

    single select

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import { Select } from "./components/select"
+
+const options = [
+  { label: 'First', value: 1},
+  { label: 'Second', value: 2},
+  { label: 'Third', value: 3},
+  { label: 'Fourth', value: 4}
+]
+
+function App() {
+  const [value, setValue] = useState<typeof options[0] | undefined>(options[0])
+  return  <>
+    <Select options={options} value={value} onChange={o => setValue(o)}/>
+  </>
+}
+
+export default App
